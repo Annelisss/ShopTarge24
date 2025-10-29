986901fe0fef1f47db103a76ea5c1fea72af28e7
Anneli Sepp <annelisepp@gmail.com>
2025-10-29 22:13:28 +0200

    Merge finalized: added Kindergarten CRUD, integrated teacher updates (AccuWeather, Layout, Program)

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,147 @@
+﻿using Nancy.Json;
+using ShopTARge24.Core.Dto.AccuWeather;
+using ShopTARge24.Core.Dto.WeatherWebClientDto;
+using ShopTARge24.Core.ServiceInterface;
+using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class WeatherForecastServices : IWeatherForecastServices
+    {
+        private readonly IConfiguration _cfg;
+
+        public WeatherForecastServices(IConfiguration cfg)
+        {
+            _cfg = cfg;
+        }
+
+        private string AccuApiKey =>
+            _cfg["AccuWeather:ApiKey"] ?? throw new InvalidOperationException("AccuWeather API key missing");
+
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            var apiKey = AccuApiKey;
+
+            var baseUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/";
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseUrl);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await httpClient.GetAsync($"{dto.CityCode}?apikey={apiKey}&details=true");
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonSerializer.Deserialize<List<AccuCityCodeRootDto>>(jsonResponse);
+
+                dto.CityName = weatherData![0].LocalizedName;
+                dto.CityCode = weatherData[0].Key;
+            }
+
+            string weatherResponse =
+                $"https://dataservice.accuweather.com/forecasts/v1/daily/1day/{dto.CityCode}?apikey={apiKey}&metric=true";
+
+            using (var clientWeather = new HttpClient())
+            {
+                var httpResponseWeather = await clientWeather.GetAsync(weatherResponse);
+                string jsonWeather = await httpResponseWeather.Content.ReadAsStringAsync();
+
+                var weatherRootDto = JsonSerializer.Deserialize<AccuLocationRootDto>(jsonWeather)!;
+
+                dto.EffectiveDate = weatherRootDto.Headline.EffectiveDate;
+                dto.EffectiveEpochDate = weatherRootDto.Headline.EffectiveEpochDate;
+                dto.Severity = weatherRootDto.Headline.Severity;
+                dto.Text = weatherRootDto.Headline.Text;
+                dto.Category = weatherRootDto.Headline.Category;
+                dto.EndDate = weatherRootDto.Headline.EndDate;
+                dto.EndEpochDate = weatherRootDto.Headline.EndEpochDate;
+
+                dto.MobileLink = weatherRootDto.Headline.MobileLink;
+                dto.Link = weatherRootDto.Headline.Link;
+
+                dto.DailyForecastsDate = weatherRootDto.DailyForecasts[0].Date;
+                dto.DailyForecastsEpochDate = weatherRootDto.DailyForecasts[0].EpochDate;
+
+                dto.TempMinValue = weatherRootDto.DailyForecasts[0].Temperature.Minimum.Value;
+                dto.TempMinUnit = weatherRootDto.DailyForecasts[0].Temperature.Minimum.Unit;
+                dto.TempMinUnitType = weatherRootDto.DailyForecasts[0].Temperature.Minimum.UnitType;
+
+                dto.TempMaxValue = weatherRootDto.DailyForecasts[0].Temperature.Maximum.Value;
+                dto.TempMaxUnit = weatherRootDto.DailyForecasts[0].Temperature.Maximum.Unit;
+                dto.TempMaxUnitType = weatherRootDto.DailyForecasts[0].Temperature.Maximum.UnitType;
+
+                dto.DayIcon = weatherRootDto.DailyForecasts[0].Day.Icon;
+                dto.DayIconPhrase = weatherRootDto.DailyForecasts[0].Day.IconPhrase;
+                dto.DayHasPrecipitation = weatherRootDto.DailyForecasts[0].Day.HasPrecipitation;
+                dto.DayPrecipitationType = weatherRootDto.DailyForecasts[0].Day.PrecipitationType;
+                dto.DayPrecipitationIntensity = weatherRootDto.DailyForecasts[0].Day.PrecipitationIntensity;
+
+                dto.NightIcon = weatherRootDto.DailyForecasts[0].Night.Icon;
+                dto.NightIconPhrase = weatherRootDto.DailyForecasts[0].Night.IconPhrase;
+                dto.NightHasPrecipitation = weatherRootDto.DailyForecasts[0].Night.HasPrecipitation;
+            }
+
+            return dto;
+        }
+
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResultWebClient(AccuLocationWeatherResultDto dto)
+        {
+            var apiKey = AccuApiKey;
+
+            string url =
+                $"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={apiKey}&q={dto.CityName}";
+
+            using (var client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                var accuResult = new JavaScriptSerializer()
+                    .Deserialize<List<AccuLocationRootWebClientDto>>(json)!;
+
+                dto.CityName = accuResult[0].LocalizedName;
+                dto.CityCode = accuResult[0].Key;
+            }
+
+            string urlWeather =
+                $"https://dataservice.accuweather.com/forecasts/v1/daily/1day/{dto.CityCode}?apikey={apiKey}&metric=true";
+
+            using (var client = new WebClient())
+            {
+                string json = client.DownloadString(urlWeather);
+                var weatherRootDto = new JavaScriptSerializer()
+                    .Deserialize<AccuWeatherRootWebClientDto>(json)!;
+
+                dto.EffectiveDate = weatherRootDto.Headline.EffectiveDate;
+                dto.EffectiveEpochDate = weatherRootDto.Headline.EffectiveEpochDate;
+                dto.Severity = weatherRootDto.Headline.Severity;
+                dto.Text = weatherRootDto.Headline.Text;
+                dto.Category = weatherRootDto.Headline.Category;
+                dto.EndDate = weatherRootDto.Headline.EndDate;
+                dto.EndEpochDate = weatherRootDto.Headline.EndEpochDate;
+
+                dto.MobileLink = weatherRootDto.Headline.MobileLink;
+                dto.Link = weatherRootDto.Headline.Link;
+
+                dto.DailyForecastsDate = weatherRootDto.DailyForecasts[0].Date;
+                dto.DailyForecastsEpochDate = weatherRootDto.DailyForecasts[0].EpochDate;
+
+                dto.TempMinValue = weatherRootDto.DailyForecasts[0].Temperature.Minimum.Value;
+                dto.TempMaxValue = weatherRootDto.DailyForecasts[0].Temperature.Maximum.Value;
+
+                dto.DayIcon = weatherRootDto.DailyForecasts[0].Day.Icon;
+                dto.DayIconPhrase = weatherRootDto.DailyForecasts[0].Day.IconPhrase;
+                dto.DayHasPrecipitation = weatherRootDto.DailyForecasts[0].Day.HasPrecipitation;
+                dto.DayPrecipitationType = weatherRootDto.DailyForecasts[0].Day.PrecipitationType;
+                dto.DayPrecipitationIntensity = weatherRootDto.DailyForecasts[0].Day.PrecipitationIntensity;
+
+                dto.NightIcon = weatherRootDto.DailyForecasts[0].Night.Icon;
+                dto.NightIconPhrase = weatherRootDto.DailyForecasts[0].Night.IconPhrase;
+                dto.NightHasPrecipitation = weatherRootDto.DailyForecasts[0].Night.HasPrecipitation;
+            }
+
+            return dto;
+        }
+    }
+}
