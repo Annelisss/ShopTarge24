26eb2b602d560f372f3844f08248ca1ee82538cb
Anneli Sepp <annelisepp@gmail.com>
2026-01-05 10:35:39 +0200

    Add ResetPassword view model and reset password view

--- ShopTARge24/Program.cs
+++ ShopTARge24/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddSignalR();
 builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
 builder.Services.AddScoped<IFileServices, FileServices>();
 builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
-builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+// builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
 builder.Services.AddScoped<IEmailServices, EmailServices>();
 
 builder.Services.AddHttpClient<IChuckNorrisServices, ChuckNorrisServices>();
